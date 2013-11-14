655f63c8b500faf47bcef11edff5e4857924e58b
Beto Esquivel <beto.esquivel.p@gmail.com>
2013-11-14 13:57:43 -0600

    Primer commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  aplicacion que a partir de un archivo con comentarios genera un 
+ *    archivo con los comentarios clasificados para los departamentos a los que va 
+ *    dirigido.
+ *
+ *        Version:  1.0
+ *        Created:  11/14/2013 01:46:52 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+
+	return 0;
+}
